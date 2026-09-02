6506a59c7589abd907fb35279f882eec6331cdb6
Deepakkumar02Github <113659959+Deepakkumar02Github@users.noreply.github.com>
2026-09-02 11:58:53 +0530

    Create script.bat

---
+++ resources/script.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo Hello from Windows Batch
+echo Current user:
+whoami
+
+echo Computer name:
+hostname
+
+echo Current directory:
+cd
+
+echo Files:
+dir
+
+echo Current date:
+date /t
+
+echo Current time:
+time /t
